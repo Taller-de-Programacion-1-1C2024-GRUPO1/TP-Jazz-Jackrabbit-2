6b8111b8f28bb35d2d7b4561d7e076bc097d5dd9
Juampi <juampiamboage18@gmail.com>
2024-05-25 11:42:28 -0300

    feat:     se lee correctamente el archivo yml de animaciones

---
+++ client_src/animation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by juampi on 24/05/24.
+//
+
+#include "animation.h"
---
+++ client_src/animation.h
@@ -0,0 +1,16 @@
+//
+// Created by juampi on 24/05/24.
+//
+
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+
+
+class animation {
+
+};
+
+
+
+#endif //ANIMATION_H
