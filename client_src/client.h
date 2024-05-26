cb77fe269921eaa9bef5108aefb190caa4db6e6b
iankvdh <ivon@fi.uba.ar>
2024-05-26 14:27:06 -0300

    feat: arquitecutra del cliente     (client_lobby, client_manager, client_renderer)

--- client_src/client.h
+++ client_src/client.h
@@ -1,40 +1,26 @@
-#ifndef CLIENTE_H
-#define CLIENTE_H
+#ifndef CLIENT_H
+#define CLIENT_H
 
 #include <fstream>
 #include <string>
 #include <unordered_map>
 #include <utility>
 
-#include "../common_src/common_socket.h"
-#include "../common_src/constants.h"
-#include "../common_src/protocol.h"
-
-#include "client_parser.h"
-#include "client_receiver.h"
-#include "client_sender.h"
+#include "client_lobby.h"
+#include "client_renderer.h"
 
 
 class Client {
 private:
-    Protocol protocol;
-    Parser parser;
-    Queue<Command> q_cmds;
-    Queue<Snapshot> q_snapshots;
-    ClientSender client_sender;
-    Client_Receiver client_receiver;
-
-    bool action_handler(std::pair<uint8_t, int> result);
-    void read_handler(int num_msgs_to_read);
+    ClientLobby lobby;
+    ClientRenderer renderer;
 
 public:
     Client(const std::string& host, const std::string& service);
 
-    // Lee de entrada estandar, procesa la linea, envia informacion al servidor y recibe e imprime
-    // la respuesta
     void run();
 
-    ~Client();
+    //~Client();
 };
 
 #endif
---
+++ client_src/client_lobby.h
@@ -0,0 +1,23 @@
+#ifndef CLIENTLOBBY_H
+#define CLIENTLOBBY_H
+
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <iostream>
+
+
+class ClientLobby {
+private:
+    //QT
+
+public:
+    ClientLobby();
+
+    void run();
+
+    //~ClientLobby();
+};
+
+#endif
