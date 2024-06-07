52eb3b507f7da4e7dc5b59e5c7920cbe38b62056
isugai <isugai@fi.uba.ar>
2024-06-07 20:06:25 -0300

    fix: Se arregla en Input handling

--- client_src/client_keyboard_handler.cpp
+++ client_src/client_keyboard_handler.cpp
@@ -12,6 +12,38 @@ void KeyboardHandler::listenForCommands(bool& game_running) {
     }
     const Uint8* state = SDL_GetKeyboardState(NULL);
 
+
+
+    if (state[SDL_SCANCODE_RIGHT]) {
+        if (state[SDL_SCANCODE_LSHIFT]){
+            q_cmds.push(std::make_unique<MoveFaster>(client_id, RIGHT));
+        } else {
+            q_cmds.push(std::make_unique<Move>(client_id, RIGHT));
+        }
+    } else if (state[SDL_SCANCODE_LEFT]) {
+        if (state[SDL_SCANCODE_LSHIFT]){
+            q_cmds.push(std::make_unique<MoveFaster>(client_id, LEFT));
+        } else {
+            q_cmds.push(std::make_unique<Move>(client_id, LEFT));
+        }
+    }
+    if (state[SDL_SCANCODE_UP] ){
+        q_cmds.push(std::make_unique<Jump>(client_id, LEFT));
+    }
+    if (state[SDL_SCANCODE_SPACE]) {
+        q_cmds.push(std::make_unique<Shoot>(client_id));
+    }
+    if (state[SDL_SCANCODE_W]) {
+        // q_cmds.try_push(ChangeWeapon(client_id));
+    }
+    if (state[SDL_SCANCODE_Q] || state[SDL_SCANCODE_ESCAPE]) {
+        game_running = false;
+    } else {
+    }
+
+
+
+    /*
     if (state[SDL_SCANCODE_UP] and state[SDL_SCANCODE_RCTRL]) {  // HABILIDAD ESPECIAL JAZZ Y LLORI
         // q_cmds->push(new UpperHit(client_id, RIGHT));
         // q_cmds->push(new ShortRangeKick(client_id, LEFT));
@@ -30,7 +62,11 @@ void KeyboardHandler::listenForCommands(bool& game_running) {
         q_cmds.push(std::make_unique<Move>(client_id, LEFT));
     } else if (state[SDL_SCANCODE_UP] and state[SDL_SCANCODE_RIGHT]) {
         q_cmds.push(std::make_unique<Jump>(client_id, RIGHT));
+        q_cmds.push(std::make_unique<Move>(client_id, RIGHT));
     } else if (state[SDL_SCANCODE_UP] and state[SDL_SCANCODE_LEFT]) {
+        q_cmds.push(std::make_unique<Jump>(client_id, LEFT));
+        q_cmds.push(std::make_unique<Move>(client_id, LEFT));
+    } else if (state[SDL_SCANCODE_UP]) {
         q_cmds.push(std::make_unique<Jump>(client_id, LEFT));
     } else if (state[SDL_SCANCODE_S]) {
         q_cmds.push(std::make_unique<Shoot>(client_id));
@@ -40,6 +76,7 @@ void KeyboardHandler::listenForCommands(bool& game_running) {
         game_running = false;
     } else {
     }
+    */
 }
 
 void KeyboardHandler::setId(int id) { client_id = id; }
