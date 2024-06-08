d1565da577f9fdd5da15a2a0581daf5d126d94d5
isugai <isugai@fi.uba.ar>
2024-06-08 17:33:13 -0300

    Refactor: -Todos los objetos son solidos por abajo (Si por arriba la sumatoria no es nula tiene un techo) -Sacando numeros Magicos

--- physics_src/physical_map.cpp
+++ physics_src/physical_map.cpp
@@ -17,7 +17,8 @@ void PhysicalMap::check_colision_with_map(int pos_x, int pos_y, int width, int h
     int x2 = trunc((pos_x + width) / BLOCK_DIVISION);
     int y2 = trunc((pos_y + height) / BLOCK_DIVISION);
 
-    if (map[x0][y0] != 0 || map[x2][y2] != 0 || map[x0][y2] != 0 || map[x2][y0] != 0) {
+    if (map[x0][y0] != NULL_OBJ || map[x2][y2] != NULL_OBJ || map[x0][y2] != NULL_OBJ ||
+        map[x2][y0] != NULL_OBJ) {
         bullet->on_colision_with_map();
     }
 }
@@ -31,33 +32,34 @@ void PhysicalMap::check_colision_with_map(int pos_x, int pos_y, int width, int h
     int x2 = trunc((pos_x + width) / BLOCK_DIVISION);
     int y2 = trunc((pos_y + height) / BLOCK_DIVISION);
 
-    if (((map[x0][y2]) + (map[x1][y2]) + (map[x2][y2])) > 4) {
-        std::cout << "on floor" << std::endl;
+    if (((map[x0][y2]) + (map[x1][y2]) + (map[x2][y2])) > COLLIDER_OBJ) {
+        // std::cout << "on floor" << std::endl;
         character->is_on_floor();
     }
-    if (((map[x0][y0]) + (map[x1][y0]) + (map[x2][y0])) > 4) {
-        std::cout << "on roof" << std::endl;
+    // TODOS LOS OBJETOS QUE NO SON NULOS SON SOLIDOS POR ABAJO
+    if (((map[x0][y0]) + (map[x1][y0]) + (map[x2][y0])) != NULL_OBJ) {
+        // std::cout << "on roof" << std::endl;
         character->is_on_roof();
     }
-    if (((map[x0][y0]) + (map[x0][y1]) + (map[x0][y2])) > 4) {
-        std::cout << "on left wall" << std::endl;
+    if (((map[x0][y0]) + (map[x0][y1]) + (map[x0][y2])) > COLLIDER_OBJ) {
+        // std::cout << "on left wall" << std::endl;
         character->is_on_left_wall();
     }
-    if (((map[x2][y0]) + (map[x2][y1]) + (map[x2][y2])) > 4) {
-        std::cout << "on right wall" << std::endl;
+    if (((map[x2][y0]) + (map[x2][y1]) + (map[x2][y2])) > COLLIDER_OBJ) {
+        // std::cout << "on right wall" << std::endl;
         character->is_on_right_wall();
     }
 
 
-    if ((map[x0][y2]) == 1 || (map[x0][y1]) == 1) {
+    if ((map[x0][y2]) == DIAG_LEFT_OBJ || (map[x0][y1]) == DIAG_LEFT_OBJ) {
         character->is_on_left_slope();
-    } else if ((map[x2][y2]) == 2 || (map[x2][y1]) == 2) {
+    } else if ((map[x2][y2]) == DIAG_RIGHT_OBJ || (map[x2][y1]) == DIAG_RIGHT_OBJ) {
         character->is_on_right_slope();
     }
 
-    if (map[x2][y2] == 1 || map[x2][y1] == 1) {
+    if (map[x2][y2] == DIAG_LEFT_OBJ || map[x2][y1] == DIAG_LEFT_OBJ) {
         character->is_on_right_wall();
-    } else if (map[x0][y2] == 2 || map[x0][y1] == 2) {
+    } else if (map[x0][y2] == DIAG_RIGHT_OBJ || map[x0][y1] == DIAG_RIGHT_OBJ) {
         character->is_on_left_wall();
     }
 }
